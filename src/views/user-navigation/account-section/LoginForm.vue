429d71b1b1ce74621c96dc0af03c847617d1a838
Iordache Ana-Maria <anaiordache264@gmail.com>
2023-11-04 20:32:04 +0200

    forgot password functionality

--- src/views/user-navigation/account-section/LoginForm.vue
+++ src/views/user-navigation/account-section/LoginForm.vue
@@ -1,12 +1,18 @@
 <template>
-    <v-form class="form_container" @submit="submitForm" ref="form">
+    <v-form class="form_container" @submit.prevent="submitForm" ref="form">
         <h3 class="form_title">Login</h3>
         <v-text-field label="Email" type="email" variant="outlined" prepend-inner-icon="mdi mdi-email"
             :rules="[rules.required]" v-model="user.email"></v-text-field>
+
+        <div class="text-caption text-blue forgot_password_link" @click="dismissPasswordDialog(false)">
+            Forgot password?
+        </div>
+
         <v-text-field label="Password" :type="showPassword ? 'text' : 'password'" variant="outlined"
             prepend-inner-icon="mdi mdi-lock" :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
             @click:append-inner="showPassword = !showPassword" :rules="[rules.required]"
             v-model="user.password"></v-text-field>
+
         <v-btn type="submit" variant="tonal" color="teal-darken-1">Login</v-btn>
 
         <v-dialog v-model="showErrorDialog" persistent width="40%">
@@ -17,14 +23,32 @@
                 </v-card-text>
             </v-card>
         </v-dialog>
+
+        <v-dialog v-model="resetPassword.showForgotDialog" width="40%">
+            <v-card color="blue-grey-lighten-5" class="pa-3 text-center">
+                <v-card-title>Reset password</v-card-title>
+                <v-card-text>
+                    <ResetPasswordForm @email-sent="showConfirmation"></ResetPasswordForm>
+                </v-card-text>
+            </v-card>
+        </v-dialog>
+
+        <v-snackbar :timeout="4000" :color="resetPassword.emailSent ? 'green-lighten-1' : 'red-lighten-1'" variant="tonal" v-model="resetPassword.showEmailConfirmation">
+            <v-icon>{{ resetPassword.emailSent ? 'mdi-check-circle-outline' : 'mdi-close-circle-outline' }}</v-icon>
+            {{ resetPassword.emailSent ? 'An email to reset your password has been sent' : 'Email to reset password couldn\'t be sent' }}
+        </v-snackbar>
     </v-form>
 </template>
 
 <script>
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import ResetPasswordForm from "./ResetPasswordForm.vue";
 
 export default {
     name: "LoginForm",
+    components: {
+        ResetPasswordForm
+    },
     data() {
         return {
             user: {
@@ -37,6 +61,11 @@ export default {
             rules: {
                 required: value => !!value || 'This field is required',
             },
+            resetPassword: {
+                showForgotDialog: false,
+                showEmailConfirmation: false,
+                emailSent: false,
+            },
         }
     },
     watch: {
@@ -47,8 +76,7 @@ export default {
         }
     },
     methods: {
-        async submitForm(e) {
-            e.preventDefault();
+        async submitForm() {
             let formValidation = await this.$refs.form.validate();
             if (formValidation.valid) {
                 await this.loginUser(this.user);
@@ -75,12 +103,26 @@ export default {
                             this.errorMessage = "Invalid credentials";
                             break;
                     }
-                    
+
                     this.showErrorDialog = true;
                 });
+        },
+        dismissPasswordDialog(dismiss) {
+            this.resetPassword.showForgotDialog = !dismiss;
+        },
+        showConfirmation(emailSent) {
+            this.dismissPasswordDialog(true);
+            this.resetPassword.emailSent = emailSent;
+            this.resetPassword.showEmailConfirmation = true;
         }
     }
 }
 </script>
 
-<style></style>
+<style scoped>
+.forgot_password_link {
+    display: flex;
+    justify-content: flex-end;
+    cursor: pointer;
+}
+</style>
---
+++ src/views/user-navigation/account-section/ResetPasswordForm.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-form class="form_container" @submit.prevent="submitForm" ref="form">
+        <v-text-field label="Email" type="email" variant="outlined" prepend-inner-icon="mdi mdi-email"
+            :rules="[rules.required]" v-model="email"></v-text-field>
+
+        <v-btn type="submit" variant="tonal" color="teal-darken-1">Reset password</v-btn>
+    </v-form>
+</template>
+
+<script>
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+
+export default {
+    name: "ResetPasswordForm",
+    emits: ['email-sent'],
+    data() {
+        return {
+            email: "",
+            rules: {
+                required: value => !!value || 'This field is required',
+            },
+            showConfirmationDialog: true,
+        }
+    },
+    methods: {
+        async submitForm() {
+            let formValidation = await this.$refs.form.validate();
+            if (formValidation.valid) {
+                await this.sendResetPasswordEmail(this.email);
+            }
+        },
+        async sendResetPasswordEmail(email) {
+            const auth = getAuth();
+            sendPasswordResetEmail(auth, email)
+            .then(() => {
+                console.log("DEBUG email sent")
+                this.$emit('email-sent', true)
+            })
+            .catch(error => {
+                console.error(error);
+                this.$emit('email-sent', false)
+            });
+        }
+    }
+}
+</script>
