3d69f9d8e6a8b2b4a276ca7125033c1e70d21676
Iordache Ana-Maria <anaiordache264@gmail.com>
2023-11-04 20:43:55 +0200

    include user in user acoount page

--- src/views/user-navigation/UserAccount.vue
+++ src/views/user-navigation/UserAccount.vue
@@ -1,9 +1,18 @@
 <template>
-    account page
+    Account info for {{ currentUser }}
 </template>
 
 <script>
+import { mapStores } from 'pinia';
+import { useAuthenticationStore } from '@/pinia-stores/authenticationStore';
+
 export default {
     name: 'UserAccountPage',
+    computed: {
+        ...mapStores(useAuthenticationStore),
+        currentUser() {
+            return `${this.authenticationStore.user.name} ${this.authenticationStore.user.surname}`;
+        }
+    }
 }
 </script>
