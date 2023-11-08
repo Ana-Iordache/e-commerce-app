bc6cecb2f542fb1abd2c6e24ab9921cd9fc40dd9
Iordache Ana-Maria <anaiordache264@gmail.com>
2023-11-08 22:45:48 +0200

    get accessories and shoes

--- src/views/app-navigation/Accessories.vue
+++ src/views/app-navigation/Accessories.vue
@@ -1,9 +1,41 @@
 <template>
-    Accessories page
+    <div class="">
+        <v-row>
+            <v-col v-for="product in accessoriesProducts" :key="product.id" cols="12" sm="6" md="4">
+                <ProductCard :product="product"></ProductCard>
+            </v-col>
+        </v-row>
+    </div>
 </template>
 
 <script>
+import ProductCard from '@/components/ProductCard.vue';
 export default {
-    name: 'AccessoriesPage',
+    name: 'AccesoriesPage',
+    components: {
+        ProductCard,
+    },
+    data() {
+        return {
+            accessoriesProducts: []
+        }
+    },
+    async mounted() {
+        await this.loadAccessoriesProducts();
+    },
+    methods: {
+        loadAccessoriesProducts() {
+            return new Promise(resolve => {
+                this.axios.get("/products?category=accesories")
+                    .then(response => response.data)
+                    .then(data => this.accessoriesProducts = data)
+                    .catch(error => console.error(error))
+                    .finally(() => resolve());
+            })
+        }
+    }
 }
 </script>
+
+<style>
+</style>
--- src/views/app-navigation/Shoes.vue
+++ src/views/app-navigation/Shoes.vue
@@ -1,9 +1,41 @@
 <template>
-    Shoes page
+    <div class="">
+        <v-row>
+            <v-col v-for="product in shoesProducts" :key="product.id" cols="12" sm="6" md="4">
+                <ProductCard :product="product"></ProductCard>
+            </v-col>
+        </v-row>
+    </div>
 </template>
 
 <script>
+import ProductCard from '@/components/ProductCard.vue';
 export default {
     name: 'ShoesPage',
+    components: {
+        ProductCard,
+    },
+    data() {
+        return {
+            shoesProducts: []
+        }
+    },
+    async mounted() {
+        await this.loadShoesProducts();
+    },
+    methods: {
+        loadShoesProducts() {
+            return new Promise(resolve => {
+                this.axios.get("/products?category=shoes")
+                    .then(response => response.data)
+                    .then(data => this.shoesProducts = data)
+                    .catch(error => console.error(error))
+                    .finally(() => resolve());
+            })
+        }
+    }
 }
 </script>
+
+<style>
+</style>
