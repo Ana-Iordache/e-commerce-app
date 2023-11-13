5a8c5ec3c2186917de6181368294adc28093cd50
Iordache Ana-Maria <anaiordache264@gmail.com>
2023-11-13 20:49:42 +0200

    add/remove fav prod for accessories/shoes

--- src/views/app-navigation/Accessories.vue
+++ src/views/app-navigation/Accessories.vue
@@ -2,14 +2,17 @@
     <div class="">
         <v-row>
             <v-col v-for="product in accessoriesProducts" :key="product.id" cols="12" sm="6" md="4">
-                <ProductCard :product="product"></ProductCard>
+                <ProductCard :product="product" @favorite-changed="updateFavorites"></ProductCard>
             </v-col>
         </v-row>
     </div>
 </template>
 
 <script>
+import { mapStores } from 'pinia';
+import { useAuthenticationStore } from '@/pinia-stores/authenticationStore';
 import ProductCard from '@/components/ProductCard.vue';
+
 export default {
     name: 'AccesoriesPage',
     components: {
@@ -20,6 +23,9 @@ export default {
             accessoriesProducts: []
         }
     },
+    computed: {
+        ...mapStores(useAuthenticationStore)
+    },
     async mounted() {
         await this.loadAccessoriesProducts();
     },
@@ -28,14 +34,38 @@ export default {
             return new Promise(resolve => {
                 this.axios.get("/products?category=accesories")
                     .then(response => response.data)
-                    .then(data => this.accessoriesProducts = data)
+                    .then(async data => {
+                        this.accessoriesProducts = data;
+                        if (this.authenticationStore.user != null) {
+                            await this.loadFavoritesOfUser();
+                        }
+                    })
                     .catch(error => console.error(error))
                     .finally(() => resolve());
             })
+        },
+        loadFavoritesOfUser() {
+            return new Promise(resolve => {
+                this.axios.get(`/users/${this.authenticationStore.user.id}/favorites`)
+                    .then(response => response.data)
+                    .then(data => {
+                        let favoriteIds = data.map(fav => fav.productCode);
+                        this.accessoriesProducts = this.accessoriesProducts.map(prod => {
+                            return {
+                                ...prod,
+                                favorite: favoriteIds.indexOf(prod.code) != -1
+                            }
+                        })
+                    })
+                    .catch(error => console.error(error))
+                    .finally(() => resolve());
+            })
+        },
+        updateFavorites(productCode, value) {
+            this.accessoriesProducts.find(prod => prod.code == productCode).favorite = value;
         }
     }
 }
 </script>
 
-<style>
-</style>
+<style></style>
--- src/views/app-navigation/Shoes.vue
+++ src/views/app-navigation/Shoes.vue
@@ -2,14 +2,17 @@
     <div class="">
         <v-row>
             <v-col v-for="product in shoesProducts" :key="product.id" cols="12" sm="6" md="4">
-                <ProductCard :product="product"></ProductCard>
+                <ProductCard :product="product" @favorite-changed="updateFavorites"></ProductCard>
             </v-col>
         </v-row>
     </div>
 </template>
 
 <script>
+import { mapStores } from 'pinia';
+import { useAuthenticationStore } from '@/pinia-stores/authenticationStore';
 import ProductCard from '@/components/ProductCard.vue';
+
 export default {
     name: 'ShoesPage',
     components: {
@@ -20,6 +23,9 @@ export default {
             shoesProducts: []
         }
     },
+    computed: {
+        ...mapStores(useAuthenticationStore)
+    },
     async mounted() {
         await this.loadShoesProducts();
     },
@@ -28,14 +34,38 @@ export default {
             return new Promise(resolve => {
                 this.axios.get("/products?category=shoes")
                     .then(response => response.data)
-                    .then(data => this.shoesProducts = data)
+                    .then(async data => {
+                        this.shoesProducts = data;
+                        if (this.authenticationStore.user != null) {
+                            await this.loadFavoritesOfUser();
+                        }
+                    })
                     .catch(error => console.error(error))
                     .finally(() => resolve());
             })
+        },
+        loadFavoritesOfUser() {
+            return new Promise(resolve => {
+                this.axios.get(`/users/${this.authenticationStore.user.id}/favorites`)
+                    .then(response => response.data)
+                    .then(data => {
+                        let favoriteIds = data.map(fav => fav.productCode);
+                        this.shoesProducts = this.shoesProducts.map(prod => {
+                            return {
+                                ...prod,
+                                favorite: favoriteIds.indexOf(prod.code) != -1
+                            }
+                        })
+                    })
+                    .catch(error => console.error(error))
+                    .finally(() => resolve());
+            })
+        },
+        updateFavorites(productCode, value) {
+            this.shoesProducts.find(prod => prod.code == productCode).favorite = value;
         }
     }
 }
 </script>
 
-<style>
-</style>
+<style></style>
